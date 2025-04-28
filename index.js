a01d72f504740ac5b9e3bf353711e0fafd95d581
XShutai <100844213+XShutai@users.noreply.github.com>
2025-04-28 10:52:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post("/send-email", async (req, res) => {
 
   try {
     const response = await resend.emails.send({
-      from: "Your App <onboarding@resend.dev>",  // Resend default sandbox domain
+      from: "System Test <noreply@josephphiri.com>",  // Resend default sandbox domain
       to,
       subject,
       text,
